baa71e9d361813358e77625b12ba824344a2f35f
inmove <lisper.inmove@gmail.com>
2023-07-11 14:25:18 +0800

    更新app.py
    1. 使用api_keys池

--- src/app.py
+++ src/app.py
@@ -1,21 +1,29 @@
 # -*- coding: utf-8 -*-
 
+import random
 from concurrent import futures
 
 import grpc
 import openai
-from proto.grpc_api.grpc_chat_completion_pb2 import ChatResponse
-from proto.grpc_api.grpc_chat_completion_pb2_grpc import ChatCompletionServicer
-import proto.grpc_api.grpc_chat_completion_pb2_grpc as chat_completion_pb2_grpc
+from proto.grpc_api.grpc_chatgpt_pb2 import ChatCompletionResponse
+from proto.grpc_api.grpc_chatgpt_pb2_grpc import ChatGPTServicer
+import proto.grpc_api.grpc_chatgpt_pb2_grpc as chatgpt_pb2_grpc
 
 from submodules.utils.protobuf_helper import ProtobufHelper as PH
+from submodules.utils.sys_env import SysEnv
 
 
-class ChatCompletion(ChatCompletionServicer):
+class ChatGPTClient(ChatGPTServicer):
 
-    api_key = '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'
+    def __init__(self):
+        self.api_keys = SysEnv.get("CHAT_GPT_API_KEYS").split(",")
 
-    def Chat(self, request, context):
+    @property
+    def api_key(self):
+        return self.api_keys[
+            random.randint(0, len(self.api_keys)) % (len(self.api_keys))]
+
+    def ChatCompletion(self, request, context):
         request = PH.to_dict(request)
         response = openai.ChatCompletion.create(
             model='gpt-3.5-turbo-16k',
@@ -24,12 +32,11 @@ class ChatCompletion(ChatCompletionServicer):
             api_key=self.api_key
         )
         for chunk in response:
-            yield PH.to_obj(chunk, ChatResponse)
+            yield PH.to_obj(chunk, ChatCompletionResponse)
 
 def serve() -> None:
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=5000))
-    chat_completion_pb2_grpc.add_ChatCompletionServicer_to_server(
-            ChatCompletion(), server)
+    chatgpt_pb2_grpc.add_ChatGPTServicer_to_server(ChatGPTClient, server)
     listen_addr = "[::]:50051"
     server.add_insecure_port(listen_addr)
     print("Starting server on %s", listen_addr)
