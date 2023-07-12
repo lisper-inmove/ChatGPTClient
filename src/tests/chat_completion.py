d9c504b5d1f3e98d7a83044c117d9c3eed46ff1d
inmove <lisper.inmove@gmail.com>
2023-07-12 08:49:05 +0800

    修改test 使用 secure_channel

--- src/tests/chat_completion.py
+++ src/tests/chat_completion.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import grpc
-import api.chat_completion_pb2 as chat_completion_pb
-import api.chat_completion_pb2_grpc as chat_completion_pb_grpc
+import proto.grpc_api.grpc_chatgpt_pb2 as grpc_chatgpt_pb
+import proto.grpc_api.grpc_chatgpt_pb2_grpc as grpc_chatgpt_pb_grpc
 
 
 def run():
     print("Will try to greet world ...")
     # 1. 创建一条通道与服务端连接
-    with grpc.insecure_channel('chat.inmove.top:50051') as channel:
+    with grpc.secure_channel('chat.inmove.top:8443', grpc.ssl_channel_credentials()) as channel:
         # 2. 创建一条存根(就当作是创建了一个Hello类)
-        stub = chat_completion_pb_grpc.ChatCompletionStub(channel)
+        stub = grpc_chatgpt_pb_grpc.ChatGPTStub(channel)
         # 3. 调用服务提供的函数,并返回结果
-        for response in stub.Chat(chat_completion_pb.ChatRequest(
+        for response in stub.ChatCompletion(grpc_chatgpt_pb.ChatCompletionRequest(
             messages=[
-                chat_completion_pb.ChatMessage(
+                grpc_chatgpt_pb.ChatCompletionRequest.ChatCompletionMessage(
                     role="user",
-                    content="Hello Write a hello world using Rust"
+                    content="An solution for leetcode 1001 using Rust"
                 )
             ]
         )):
